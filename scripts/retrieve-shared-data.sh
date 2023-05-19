27fbb794e8da3a067f487f66df14a36abc817241
PierreEVEN <evenpierre@orange.fr>
2023-05-19 16:58:47 +0200

    fixed script to retrieve logs

--- scripts/retrieve-shared-data.sh
+++ scripts/retrieve-shared-data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+sudo rm -r /home/ubuntu/docker/Bidibip/saved/
 mkdir -p /home/ubuntu/docker/Bidibip/saved/
 sudo cp -r /var/lib/docker/volumes/bidibip-saved/_data/ /home/ubuntu/docker/Bidibip/saved/
 sudo mv /home/ubuntu/docker/Bidibip/saved/_data/* /home/ubuntu/docker/Bidibip/saved
