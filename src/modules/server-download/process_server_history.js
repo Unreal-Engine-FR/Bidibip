3a1d30ad105eb22423dbf431855bbf11e8427709
PierreEVEN <evenpierre@orange.fr>
2023-06-04 19:49:26 +0200

    ideas on how to generate vector db

--- src/modules/server-download/process_server_history.js
+++ src/modules/server-download/process_server_history.js
@@ -6,24 +6,34 @@ logger.init_logger()
 function string_to_unicode(input) {
     let str = ''
     for (let i = 0; i < input.length; ++i) {
-        if ((input.charCodeAt(i) >= 32 && input.charCodeAt(i) <= 127) || input.charAt(i) === '\n') {
+        if ((input.charCodeAt(i) >= 32 && input.charCodeAt(i) <= 127)) {
             str += input.charAt(i)
         }
+        else if (input.charAt(i) === '\n') // replace line break with space
+            str += ' '
     }
+
+    str = str.replaceAll(/<:.*:[0-9]*>/g, '') // remove emotes
+    str = str.replaceAll(/<[@#]!?[0-9]*>/g, '') // remove mentions
+    str = str.replaceAll(/https?:\/\/\S*/g, '')
     return str
 }
 
-function get_files() {
+function get_files(channel = null) {
 
     let discussion = []
 
-    for (const channel of fs.readdirSync(`${CONFIG.get().SAVE_DIR}/message_history`)) {
+    const channels = channel ? [channel] : fs.readdirSync(`${CONFIG.get().SAVE_DIR}/message_history`)
+
+    for (const channel of channels) {
         console.info(`Start process of channel #${channel}`)
         const channel_conversations = []
         for (const chunk of fs.readdirSync(`${CONFIG.get().SAVE_DIR}/message_history/${channel}`).sort((a, b) => Number(a.split('.')[0]) - Number(b.split('.')[0]))) {
             const data = JSON.parse(fs.readFileSync(`${CONFIG.get().SAVE_DIR}/message_history/${channel}/${chunk}`, "utf-8"))
             for (const [k, v] of Object.entries(data)) {
-                channel_conversations.push(string_to_unicode(v.t.replaceAll(/[éèê]/g, 'e').replaceAll('ç', 'c')))
+                const text = string_to_unicode(v.t.replaceAll(/[éèê]/g, 'e').replaceAll('ç', 'c'))
+                if (text.length !== 0)
+                    channel_conversations.push(/*string_to_unicode(v.u.split('#')[0]) + " : " + */text + '\n')
             }
         }
         discussion = discussion.concat(channel_conversations.reverse())
@@ -31,31 +41,12 @@ function get_files() {
 
     let string = ''
     for (const text of discussion)
-        string += `${text}\n`
+        string += `${text}`
 
     fs.writeFileSync(`${CONFIG.get().SAVE_DIR}/generated_save.txt`, string, "utf-8")
 
     console.log('Saved', discussion.length, 'messages')
 }
+get_files('543932597173223425')
 
-get_files()
-
-
-/*
-for (const chunk of fs.readdirSync(`./src/modules/server-download/test_a`)) {
-
-    const file = fs.readFileSync(`./src/modules/server-download/test_a/${chunk}`, "utf-8")
-    console.log("loaded", chunk)
-
-    const string = file.match(/(.|[\r\n]){1,1000000}/g)
-    fs.appendFileSync(`./src/modules/server-download/test_b/${chunk}`, '', "utf-8")
-    let i = 1
-    for (const s of string) {
-        const fixed = string_to_unicode(s)
-        console.log('write chunk', i, 'of', string.length)
-        i += 1
-        fs.appendFileSync(`./src/modules/server-download/test_b/${chunk}`, fixed, "utf-8")
-    }
-    console.log("wrote", chunk)
-}
- */
+//get_files('447767547597684738')
